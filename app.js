7ed11d3c25faf2e50ac16499d5cc214b82b0628c
Anurag Srivastav <anuragsrivastav@Anurags-MacBook-Air.local>
2022-02-17 23:51:06 +0530

    md5

--- app.js
+++ app.js
@@ -2,7 +2,8 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const mongoose = require("mongoose");
-const encrypt = require("mongoose-encryption");
+const md5 = require("md5")
+
 require('dotenv').config();
 
 const app = express();
@@ -18,9 +19,7 @@ const userSchema =  new mongoose.Schema ({
     password: String
 });
 
-const secret = process.env.SECRET;
 
-userSchema.plugin(encrypt,{secret: secret, encryptedFields: ["password"]});
 
 const User = mongoose.model("User", userSchema);
 
@@ -39,7 +38,7 @@ app.get("/register", function(req,res){
 app.post("/register", function(req,res){
     const newUser = new User({
         email: req.body.username,
-        password: req.body.password
+        password: md5(req.body.password)
     });
 
     newUser.save(function(err){
@@ -53,7 +52,7 @@ app.post("/register", function(req,res){
 
 app.post("/login", function(req,res){
     const username = req.body.username;
-    const password = req.body.password;
+    const password = md5(req.body.password);
 
     User.findOne({email: username}, function(err , foundUser){
         if(err){
